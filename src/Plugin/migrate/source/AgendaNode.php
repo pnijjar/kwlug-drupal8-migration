898ed7d661933ee0ce3a9c797e3d18b897364875
Paul Worthless Nijjar <pnijjar@alumni.uwaterloo.ca>
2016-12-26 02:49:36 -0500

    Include agenda extra fields

--- src/Plugin/migrate/source/AgendaNode.php
+++ src/Plugin/migrate/source/AgendaNode.php
@@ -50,6 +50,7 @@ class AgendaNode extends D6Node {
     $presentation_info = $query->execute()
       ->fetchAll();
 
+
     if ($presentation_info) { 
 
       foreach ($presentation_info as $p) { 
@@ -62,7 +63,7 @@ class AgendaNode extends D6Node {
         } else { 
           $title_so_far = $ptitle;
         } // end if 
-        $row->setSourceProperty('presentation_title', $ptitle);
+        $row->setSourceProperty('presentation_title', $title_so_far);
 
         // How do I add multiple NIDs?
         $row->setSourceProperty('presentation_nid', $p['nid']);
@@ -72,7 +73,7 @@ class AgendaNode extends D6Node {
         $pbody = $p['body'];
 
         if ($body_so_far) { 
-          $body_so_far = $body_so_far . "\n\n" . $pbody;
+          $body_so_far = $body_so_far . "\n\n* * *\n" . $pbody;
         } else { 
           $body_so_far = $pbody;
         } // end if body
@@ -89,10 +90,69 @@ class AgendaNode extends D6Node {
     } // end if presentation_info exists
 
 
+    // I do not know why this stuff doesn't migrate itself, 
+    // but whatever.
+    $query_agenda = $this->select('content_type_agenda','c')
+      ->fields('c', ['field_emcee_uid', 'field_date_value', 
+          'field_location_nid'])
+      ->condition('c.nid', $nid, '=');
+    $agenda_info = $query_agenda->execute()
+      ->fetchAll();
+
+    if ($agenda_info) { 
+
+      // There SHOULD be only one row. I guess we are taking the last 
+      // value if there are multiple. 
+      // Lots of these will be NULL, though. 
+      // Also we just want to append to the body if there is an emcee.
+      foreach ($agenda_info as $a) { 
+        $row->setSourceProperty('meeting_date', $a['field_date_value']);
+        $row->setSourceProperty('emcee_uid', $a['field_emcee_uid']);
+        $row->setSourceProperty('meeting_location_nid', $a['field_location_nid']);
+
+        if ($a['field_emcee_uid']) { 
+          $body_so_far = $row->getSourceProperty('body');
+
+
+          $query_emcee = $this->select('users','u')
+            ->fields('u', ['name'])
+            ->condition('u.uid', $a['field_emcee_uid']);
+
+          $emcee_info = $query_emcee->execute()->fetchAssoc();
+          $emcee_name = "User " . $a['field_emcee_uid'];
+
+          if ($emcee_info) { 
+            $emcee_name = $emcee_info['name'];
+          } // end emcee_info
+
+          $extra_text = "\n\nThe host for this meeting was " 
+              . $emcee_name
+              . ".";
+
+          if ($body_so_far) { 
+            $body_so_far = $body_so_far . "\n* * *\n" . $extra_text;
+          } else { 
+            $body_so_far = $extra_text;
+          } // end if body_so_far
+
+          $row->setSourceProperty('body', $body_so_far);
+
+
+        } // end if emcee exists
+
+
+      } // end loop $a
+    } // end if agenda_info
+
     if ($nid >= $DEBUG_NID_START  && $nid <= $DEBUG_NID_END ) { 
       print_r("\n\row is\n");
       print_r($row);
-    } 
+      print_r("\n\agenda_info is\n");
+      print_r($agenda_info);
+
+      print_r("\n\emcee_info is\n");
+      print_r($emcee_info);
+    } // end if debug
 
     // TODO: Look for associated FLOSS Fund nominees 
     return parent::prepareRow($row);
@@ -118,6 +178,9 @@ class AgendaNode extends D6Node {
       'presentation_title' => $this->t('Presentation Title'), 
       'presentation_nid' => $this->t('Presentation Node (ugh)'), 
       'floss_fund_nominee_nid' => $this->t('FLOSS Fund Nominee NID'),
+      'meeting_date' => $this->t('Meeting Date'),
+      'meeting_location_nid' => $this->t('Meeting Location NID'),
+      'emcee_uid' => $this->t('Meeting MC UID'),
       'podcast' => $this->t('Podcast'),
       'vidcast' => $this->t('Vidcast'),
     );
