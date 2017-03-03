3a571321f43910cef6104b84c6e0db3b2af643ee
Paul Worthless Nijjar <pnijjar@alumni.uwaterloo.ca>
2017-03-03 00:55:17 -0500

    Uninstall migrate_ database tables. Yikes!

--- bin/clean-migrate-tools.sh
+++ bin/clean-migrate-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Uninstall modules used only for migrations. Order is important!
+# BEWARE: This will also drop migrate_map_* and migrate_message_* .
 
 INSTALLPATH=/home/linuxuser/kwlug-drupal-v05/web
 
@@ -18,6 +19,15 @@ PACKAGES=" \
 for i in $PACKAGES
 do
   drush pm-uninstall ${i} --yes
+done
+
+migratemap=`drush sqlq "show tables like 'migrate_map_%'"`
+migratemsg=`drush sqlq "show tables like 'migrate_message_%'"`
+
+for i in $migratemap $migratemsg
+do
+  echo Dropping table $i
+  drush sqlq "drop table $i"
 done
 
 popd
